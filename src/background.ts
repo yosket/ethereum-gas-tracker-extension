6c9cd3e1f64fa077ed60711714a70e7821ef3da7
Yosuke Tanaka <yosket87@gmail.com>
2021-09-04 13:22:06 +0900

    Imprement

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,32 @@
 import { browser } from 'webextension-polyfill-ts'
 
-browser.action.setBadgeBackgroundColor({ color: 'red' })
-browser.action.setBadgeText({ text: '1234' })
+start()
+
+async function start() {
+  await update()
+  setInterval(update, 5000)
+  console.info('[Ethereum Gas Tracker] Started!')
+}
+
+async function update() {
+  try {
+    const {
+      result: { ProposeGasPrice }
+    } = await getGasData()
+    browser.action.setBadgeText({ text: ProposeGasPrice })
+    browser.action.setBadgeBackgroundColor({ color: '#18A0FB' })
+  } catch (e) {
+    console.error(e)
+    browser.action.setBadgeText({ text: '!' })
+    browser.action.setBadgeBackgroundColor({ color: '#F24D4D' })
+  }
+}
+
+async function getGasData() {
+  const res = await fetch(
+    `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${
+      import.meta.env.VITE_ETHERSCAN_API_KEY_TOKEN
+    }`
+  )
+  return await res.json()
+}
